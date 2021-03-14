3c798903aaaac97fc099bb86b31f861265ed1535
Francis Ignacio <48373989+noeyignacio@users.noreply.github.com>
2021-03-14 11:57:30 +0800

    :small_red_triangle: UIKit CDN added.
    UIKit CDN `https://getuikit.com/docs/installation` added.
    Navbar Component added.

---
+++ client/src/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<nav class="uk-navbar-container uk-margin" uk-navbar>
+    <div class="uk-navbar-center">
+        <div class="uk-navbar-center-left"></div>
+        <a class="uk-navbar-item uk-logo" href="/">Expenses Tracker</a>
+        <div class="uk-navbar-center-right"></div>
+    </div>
+</nav>
